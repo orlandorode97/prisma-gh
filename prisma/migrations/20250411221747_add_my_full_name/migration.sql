1c899df8dcac3396affe8407fd4aaa5aa550ce30
orlando romo <orlando.romo.97@hotmail.com>
2025-04-11 16:37:51 -0600

    remove unexisting index

--- prisma/migrations/20250411221747_add_my_full_name/migration.sql
+++ prisma/migrations/20250411221747_add_my_full_name/migration.sql
@@ -4,8 +4,6 @@
   - Added the required column `my_full_name` to the `model` table without a default value. This is not possible if the table is not empty.
 
 */
--- DropIndex
-DROP INDEX `model_email_key` ON `model`;
 
 -- AlterTable
 ALTER TABLE `model` ADD COLUMN `my_full_name` VARCHAR(191) NOT NULL;
