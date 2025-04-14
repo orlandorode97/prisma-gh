50c4993be8ae2dc66fe6b620df65b3ab07d8591e
orlando romo <orlando.romo.97@hotmail.com>
2025-04-13 22:06:54 -0600

    more migrations

--- prisma/migrations/20250414040624_add_new_field/migration.sql
+++ prisma/migrations/20250414040624_add_new_field/migration.sql
@@ -3,5 +3,9 @@
 
   - Added the required column `this_is_my` to the `model` table without a default value. This is not possible if the table is not empty.
 
+*/
+-- DropIndex
+DROP INDEX `model_email_key` ON `model`;
+
 -- AlterTable
 ALTER TABLE `model` ADD COLUMN `this_is_my` VARCHAR(191) NOT NULL;
