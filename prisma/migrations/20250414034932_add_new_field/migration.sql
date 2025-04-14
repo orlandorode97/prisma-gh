d792fea9571e4232c2e6c74ec3c9851389eb44c6
orlando romo <orlando.romo.97@hotmail.com>
2025-04-13 22:02:09 -0600

    update rollback

--- prisma/migrations/20250414034932_add_new_field/migration.sql
+++ prisma/migrations/20250414034932_add_new_field/migration.sql
@@ -4,4 +4,4 @@
   - Added the required column `this_is_my` to the `model` table without a default value. This is not possible if the table is not empty.
 
 -- AlterTable
-ALTER TABLE `model` ADD COLUMN `this_is_my` VARCH(191) NOT NULL;
+ALTER TABLE `model` ADD COLUMN `this_is_my` VARCHAR(191) NOT NULL;
