b6efee973494c9cd6fb3aa402657549afe56e17a
orlando romo <orlando.romo.97@hotmail.com>
2025-04-14 12:17:43 -0600

    init

--- prisma/migrations/0_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE `my_new_table` (
-    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-    `created_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
