bca43729f6ff5bf1e956d0ff738d88fd43842838
orlando romo <orlando.romo.97@hotmail.com>
2025-04-13 21:50:21 -0600

    add new field un purpose

---
+++ prisma/migrations/20250414034932_add_new_field/migration.sql
@@ -0,0 +1,7 @@
+/*
+  Warnings:
+
+  - Added the required column `this_is_my` to the `model` table without a default value. This is not possible if the table is not empty.
+
+-- AlterTable
+ALTER TABLE `model` ADD COLUMN `this_is_my` VARCH(191) NOT NULL;
